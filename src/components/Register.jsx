74e3412c8a52e5783cc35764397f5e74557c09e6
Raúl <raul.rodriguez.c.2012@gmail.com>
2024-11-10 00:48:39 -0300

    Mensaje de éxito

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,72 +1,75 @@
 import { useState } from "react";
 
 const Register = () => {
-    const [email, setEmail] = useState('');
-    const [contraseña, setContraseña] = useState('');
-    const [confirmar, setConfirmar] = useState('');
-    const [error, setError] = useState('');
-  
-    const validarDatos = (e) => {
-      e.preventDefault();
-      
+  const [email, setEmail] = useState("");
+  const [contraseña, setContraseña] = useState("");
+  const [confirmar, setConfirmar] = useState("");
+  const [error, setError] = useState("");
+  const [exito, setExito] = useState("");
 
-      if (!email.trim() || !contraseña.trim() || !confirmar.trim()) {
-        setError('Todos los campos son obligatorios.');
-        return;
-      }
-      if (contraseña.length < 6) {
-        setError('La contraseña debe tener más de 6 caracteres.');
-        return;
-      }
-      if (contraseña !== confirmar) {
-        setError('Las contraseñas no coinciden.');
-        return;
-      }
-      if (!email.includes('@') || !email.includes('.')) {
-        setError('El email debe tener una @ y un punto.');
-        return;
-      }
-  
+  const validarDatos = (e) => {
+    e.preventDefault();
+    setError("");
+    setExito("");
 
-      setError('');
-    };
-  
+    if (!email.trim() || !contraseña.trim() || !confirmar.trim()) {
+      setError("Todos los campos son obligatorios.");
+      return;
+    }
+    if (contraseña.length < 6) {
+      setError("La contraseña debe tener más de 6 caracteres.");
+      return;
+    }
+    if (contraseña !== confirmar) {
+      setError("Las contraseñas no coinciden.");
+      return;
+    }
+    if (!email.includes("@") || !email.includes(".")) {
+      setError("El email debe tener una @ y un punto.");
+      return;
+    }
 
-    return (
-      <>
-        <form className="formulario" onSubmit={validarDatos}>
-          <div className="formGroup">
-            <label>Email</label>
-            <input 
-              type="email" 
-              value={email} 
-              onChange={(e) => setEmail(e.target.value)} 
-            />
-          </div>
-          <div className="formGroup">
-            <label>Contraseña</label>
-            <input 
-              type="password" 
-              value={contraseña} 
-              onChange={(e) => setContraseña(e.target.value)} 
-            />
-          </div>
-          <div className="formGroup">
-            <label>Confirmar Contraseña</label>
-            <input 
-              type="password" 
-              value={confirmar} 
-              onChange={(e) => setConfirmar(e.target.value)} 
-            />
-          </div>
-          
-          {/* Mostrar error si existe */}
-          {error && <p className="error">{error}</p>}
-          
-          <button type="submit">Enviar</button>
-        </form>
-      </>
-    );
+    setExito("¡Registro Exitoso!");
   };
 
-  export default Register
+  return (
+    <>
+      <form className="formulario" onSubmit={validarDatos}>
+        <div className="formGroup">
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="formGroup">
+          <label>Contraseña</label>
+          <input
+            type="password"
+            value={contraseña}
+            onChange={(e) => setContraseña(e.target.value)}
+          />
+        </div>
+        <div className="formGroup">
+          <label>Confirmar Contraseña</label>
+          <input
+            type="password"
+            value={confirmar}
+            onChange={(e) => setConfirmar(e.target.value)}
+          />
+        </div>
+
+        {/* Mostrar error si existe */}
+        {error && <p className="error">{error}</p>}
+
+        {/* Mostrar mensaje de éxito si corresponde */}
+        {exito && <p className="exito">{exito}</p>}
+
+        <button type="submit">Enviar</button>
+      </form>
+    </>
+  );
+};
+
+export default Register;
